dbf9103e946dc034135b8a0b83ca64cf90394551
stf04759 <stf04759@MAC1323.local>
2024-04-23 14:20:20 +0900

    test key vault

--- function_app.py
+++ function_app.py
@@ -1,7 +1,17 @@
 import azure.functions as func
 import logging
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+KeyVaultName = "test-rag-index"
+KVUri = f"https://{KeyVaultName}.vault.azure.net"
+
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=KVUri, credential=credential)
+
+secretName = "AZURE-OPENAI-KEY"
+retrieved_secret = client.get_secret(secretName)
 
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
@@ -20,6 +30,6 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             f"This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.{retrieved_secret}",
              status_code=200
         )
